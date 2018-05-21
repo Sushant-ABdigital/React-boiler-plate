e322d77b5e9f426f84d677394ecf48b3679b781f
Sushant <34920404+Sushant-ABdigital@users.noreply.github.com>
2018-05-21 20:05:19 +0530

    Boiler-plate for React Project
    Boiler-plate for React Project

---
+++ src/app.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import 'normalize.css/normalize.css';
+
+import './styles/styles.scss';
+
+ReactDOM.render(<p>THis is the boiler plate</p>, document.getElementById("app"));
